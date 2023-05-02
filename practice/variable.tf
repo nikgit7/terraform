2599bbfd43fa2cc995756fc4701a492c5989a90c
Nikhil Sharma <130028362+nikgit7@users.noreply.github.com>
2023-05-02 10:19:05 +0530

    Create variable.tf

---
+++ practice/variable.tf
@@ -0,0 +1,10 @@
+variable location {
+  description = "Location of server"
+  type        = string
+  default     = "finland"
+}
+variable server_name {
+  description = "Name of server"
+  type        = string
+  default     = "primary-app-server"
+}
